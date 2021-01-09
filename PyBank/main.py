4cc3a975a4d04c983afb928bae272dfff1add7f1
jaryan77 <j.alexryan178@gmail.com>
2021-01-09 17:13:51 -0600

    PyBank Complete

--- PyBank/main.py
+++ PyBank/main.py
@@ -10,10 +10,6 @@ profit = []
 monthly_change = []
 month = []
 
-# def print_table(budget_csv):
-#      month = str(budget_csv[0])
-#      profits = int(budget_csv[1])
-
 #initialize variables
 count = 0
 total_profits = 0
@@ -21,116 +17,62 @@ total_change_profits = 0
 initial_profit = 0
 
 
-
-
-#Block of code for reading csv (warm-up)
+#Read into csv
 with open(budget_csv) as csvfile:
-     #csvreader = csv.DictReader(csvfile, delimiter=',')
-     csvreader = csv.reader(csvfile, delimiter=',')
-     header = next(csvreader)
-     
-     # def columns(csvreader):
-     #      months = str(csvreader[0])
-     #      profits = int(csvreader[1])
+    csvreader = csv.reader(csvfile, delimiter=',')
+    header = next(csvreader)
 
+    for row in csvreader:
 
-     # header = next(csvreader)
-     # print(f"Financial Analysis:")
-     # print("------------------------------")
+        #use count func. to count rows minus header
+        count = count + 1
 
-     for row in csvreader:
+        #append months to row[0] to collect differences in prof.
+        month.append(row[0])
 
-          #use count func. to count rows minus header
-          count = count + 1
+         #append profit col. then calc. total_profit
+        profit.append(row[1])
+        total_profits = total_profits + int(row[1])
 
-          #append months to row[0] to collect differences in prof.
-          month.append(row[0])
+        #calc avg change in prof. month/month
+        final_profit = int(row[1])
+        montly_change_profits = final_profit - initial_profit
 
-          #append profit col. then calc. total_profit
-          profit.append(row[1])
-          total_profits = total_profits + int(row[1])
+        #store monthly changes in list
+        monthly_change.append(montly_change_profits)
 
-          #calc avg change in prof. month/month
-          final_profit = int(row[1])
-          montly_change_profits = final_profit - initial_profit
+        total_change_profits = total_change_profits + montly_change_profits
+        initial_profit = final_profit
 
-          #store monthly changes in list
-          monthly_change.append(montly_change_profits)
+        #calc avg change in profits
+        average_change_profits = (total_change_profits/count)
+        #Find the max and min change in profits and the corresponding dates these changes were obeserved
+        greatest_increase_profits = max(monthly_change)
+        greatest_decrease_profits = min(monthly_change)
 
-          total_change_profits = total_change_profits + montly_change_profits
-          initial_profit = final_profit
-
-          #calc avg change in profits
-          average_change_profits = (total_change_profits/count)
-          #Find the max and min change in profits and the corresponding dates these changes were obeserved
-          greatest_increase_profits = max(monthly_change)
-          greatest_decrease_profits = min(monthly_change)
-
-          increase_date = month[monthly_change.index(greatest_increase_profits)]
-          decrease_date = month[monthly_change.index(greatest_decrease_profits)]
-      
-     # print("----------------------------------------------------------")
-     # print("Financial Analysis")
-     # print("----------------------------------------------------------")
-     # print("Total Months: " + str(count))
-     # print("Total Profits: " + "$" + str(total_profits))
-     # print("Average Change: " + "$" + str(int(average_change_profits)))
-     # print("Greatest Increase in Profits: " + str(increase_date) + " ($" + str(greatest_increase_profits) + ")")
-     # print("Greatest Decrease in Profits: " + str(decrease_date) + " ($" + str(greatest_decrease_profits)+ ")")
-     # print("----------------------------------------------------------")
-
-     greatest_increase = [increase_date, greatest_increase_profits]
-     greatest_decrease = [decrease_date, greatest_decrease_profits]
-     summary_table = {"Total Months":count,
-          "Total Profits": total_profits,
-          "Average Change": average_change_profits,
-          "Greatest Increase in Profits": greatest_increase,
-          "Greatest Decrease in Profits": greatest_decrease
-          }
-     print(summary_table)
-     # month_count = 0
-
-     # for row in csvreader:
-
-     # total_months = sum(1 for row in csvreader)
-     # print(f"Total Months: {total_months}")
-
-     # total_profits = len(profits)
-     # print(f"Total Profits: {total_profits}")
-
-          
-               
-     # profits = {}
-     # profits[sum(csvreader)[1]]
-     # print(f"Total Profits: {profits}")
-
-     #anything
-
-     
-
-
-
-
-
-     #create a dictionary for each column
-
-     #conditional needed to calc avg change in profit/loss
-     #for row in csvreader:
-
-     #Add up values of Profit/Losses column, then average the sum
-     # for row in csvreader:
-     #      profits = float(sum(row[1]))
-     #      print(f"Total Profits: {profits})
-
-     # for row in csvreader:
-     #      print(row)
-
-         
-
-
-
-
+        increase_date = month[monthly_change.index(greatest_increase_profits)]
+        decrease_date = month[monthly_change.index(greatest_decrease_profits)]
     
+    #print results 
+    print("----------------------------------------------------------")
+    print("Financial Analysis")
+    print("----------------------------------------------------------")
+    print("Total Months: " + str(count))
+    print("Total Profits: " + "$" + str(total_profits))
+    print("Average Change: " + "$" + str(int(average_change_profits)))
+    print("Greatest Increase in Profits: " + str(increase_date) + " ($" + str(greatest_increase_profits) + ")")
+    print("Greatest Decrease in Profits: " + str(decrease_date) + " ($" + str(greatest_decrease_profits)+ ")")
+    print("----------------------------------------------------------")
 
-
+with open('analysis/finalcial_analysis.txt', 'w', newline='') as textfile:
+    writer = csv.writer(textfile)
+    writer.writerow(["----------------------------------------------------------"])
+    writer.writerow(["Financial Analysis"])
+    writer.writerow(["----------------------------------------------------------"])
+    writer.writerow(["Total Months: " + str(count)])
+    writer.writerow(["Total Profits: " + "$" + str(total_profits)])
+    writer.writerow(["Average Change: " + "$" + str(int(average_change_profits))])
+    writer.writerow(["Greatest Increase in Profits: " + str(increase_date) + " ($" + str(greatest_increase_profits) + ")"])
+    writer.writerow(["Greatest Decrease in Profits: " + str(decrease_date) + " ($" + str(greatest_decrease_profits)+ ")"])
+    writer.writerow(["----------------------------------------------------------"])
 
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,30 +3,44 @@ import os
 import csv
 
 #assign csv file to path
-poll_csv = os.path.join('Resources', 'election_data.csv')
+# poll_csv = os.path.join('Resources', 'election_data.csv')
 
 #define variables
-total_votes = 0
+# total_votes = 0
+# candidate_name = []
+# #d = {candidate, count of votes} ------ d. values -> list of values in a dict.
+# d = {}
+# with open(poll_csv) as csvfile:
+#     csvreader = csv.reader(csvfile, delimiter=',')
+#     header = next(csvreader)
 
-#d = {candidate, count of votes} ------ d. values -> list of values in a dict.
-d = {}
-with open(poll_csv) as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=',')
-    header = next(csvreader)
+#     for row in csvreader:
 
-    for row in csvreader:
+#         total_votes += 1
+#         #Will not neet line 21 when replacing 'khan' w/ variable for candidate
+#         # if row[2] == candidate_name:
+#         #     if cadidate_name in d.keys():
+#         #         d[candidate_name] += 1
+#         #     if candidate_name not in d.keys():
+#         #         d[candidate_name] = 1
 
-        total_votes += 1
-        #Will not neet line 21 when replacing 'khan' w/ variable for candidate
-        if row[2] == 'Khan':
-            if 'Khan' in d.keys():
-                d['Khan'] += 1
-            if 'Khan' not in d.keys():
-                d['Khan'] = 1
+# print(d)
 
-print(d)
+total_vote = 0
+candidates = {}
+poll_csv = os.path.join('Resources', 'election_data.csv')
+with open(poll_csv, newline="") as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter = ",")
+    csv_header = next(csv_reader)
+    for row in csv_reader:
+        total_vote += 1
+        candidate_name = row[2]
+        if candidate_name not in candidates:
+            candidates.update({candidate_name : 1})
+        else:
+            candidates[candidate_name] = candidates[candidate_name] + 1 
 
-        
+    print(candidates)
 
 
         
