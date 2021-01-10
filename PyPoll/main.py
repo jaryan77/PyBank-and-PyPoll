ff35277f329f6a193dcf8ffc8106fadcda5969e1
jaryan77 <j.alexryan178@gmail.com>
2021-01-09 18:39:54 -0600

    final submission

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,44 +3,60 @@ import os
 import csv
 
 #assign csv file to path
-# poll_csv = os.path.join('Resources', 'election_data.csv')
+poll_csv = os.path.join('Resources', 'election_data.csv')
 
 #define variables
-# total_votes = 0
-# candidate_name = []
-# #d = {candidate, count of votes} ------ d. values -> list of values in a dict.
-# d = {}
-# with open(poll_csv) as csvfile:
-#     csvreader = csv.reader(csvfile, delimiter=',')
-#     header = next(csvreader)
+total_votes = 0
 
-#     for row in csvreader:
+#d = {candidate, count of votes} ------ d. values -> list of values in a dict.
+candidate_options = []
+candidate_votes = {}
+winning_candidate = ""
+winning_count = 0
 
-#         total_votes += 1
-#         #Will not neet line 21 when replacing 'khan' w/ variable for candidate
-#         # if row[2] == candidate_name:
-#         #     if cadidate_name in d.keys():
-#         #         d[candidate_name] += 1
-#         #     if candidate_name not in d.keys():
-#         #         d[candidate_name] = 1
+with open(poll_csv) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    header = next(csvreader)
 
-# print(d)
+    
 
-total_vote = 0
-candidates = {}
-poll_csv = os.path.join('Resources', 'election_data.csv')
-with open(poll_csv, newline="") as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter = ",")
-    csv_header = next(csv_reader)
-    for row in csv_reader:
-        total_vote += 1
+    for row in csvreader:
+        total_votes += 1
+
         candidate_name = row[2]
-        if candidate_name not in candidates:
-            candidates.update({candidate_name : 1})
-        else:
-            candidates[candidate_name] = candidates[candidate_name] + 1 
+        if candidate_name not in candidate_options:
+            candidate_options.append(candidate_name)
+            candidate_votes[candidate_name] = 0
+        candidate_votes[candidate_name] += 1
 
-    print(candidates)
+    print("Election Results:")
+    print("-------------------------------")
+    print("Total Votes: ", total_votes)
+    print("-------------------------------")
+
+    for candidate in candidate_votes:
+        votes = candidate_votes.get(candidate)
+        vote_percent = float(votes)/float(total_votes)*100
+    
+        if (votes > winning_count):
+            winning_count = votes
+            winning_candidate = candidate
+
+        voter_output = f"{candidate}: {vote_percent:.3f}% ({votes})\n"
+        print(voter_output, end="")
+
+    print("-------------------------------")
+    print("Winner: ", winning_candidate)
+
+with open('analysis/election_results.txt', 'w', newline='') as textfile:
+    writer = csv.writer(textfile)
+    writer.writerow(["Election Results:"])
+    writer.writerow(["-------------------------------"])
+    writer.writerow(["Total Votes: " + str(total_votes)])    
+    writer.writerow(["-------------------------------"])
+    writer.writerow([candidate_votes])
+    writer.writerow(["-------------------------------"])
+    writer.writerow(["Winner: " + winning_candidate])
+    
 
 
-        
